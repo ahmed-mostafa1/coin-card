79300327c0c023afba991dbce9ad97adc9bda019
ahmed-mostafa1 <ahmed.code.dev@gmail.com>
2026-03-25 02:00:06 +0200

    Show contact page in main sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -216,11 +216,12 @@
                 <span>{{ __('messages.terms_of_use') }}</span>
             </a>
 
+            @endif
+
             <a href="{{ route('contact-us.show') }}" class="flex items-center gap-3 rounded-lg border border-slate-400 dark:border-slate-700 bg-white dark:bg-slate-800 px-4 py-3 text-sm font-medium text-slate-700 dark:text-slate-200 transition hover:bg-slate-50 dark:hover:bg-slate-700">
                 <i class="fa-solid fa-envelope text-orange-400 w-5"></i>
                 <span>{{ __('messages.contact_page') }}</span>
             </a>
-            @endif
             
             @if($sharedWhatsappLink)
              <a href="{{ $sharedWhatsappLink }}" target="_blank" class="flex items-center gap-3 rounded-lg border border-slate-400 dark:border-slate-700 bg-white dark:bg-slate-800 px-4 py-3 text-sm font-medium text-slate-700 dark:text-slate-200 transition hover:bg-slate-50 dark:hover:bg-slate-700">
