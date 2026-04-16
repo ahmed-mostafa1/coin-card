8b5e269f990149a6e3be9cb1fe89d18bdbbfee29
Ahmed Mostafa <ahmed.code.dev@gmail.com>
2026-04-16 14:01:50 +0200

    favicon

--- resources/views/partials/seo-head.blade.php
+++ resources/views/partials/seo-head.blade.php
@@ -84,8 +84,10 @@
     <meta name="keywords" content="{{ $metaKeywords }}">
 @endif
 <link rel="canonical" href="{{ $metaCanonical }}">
-<link rel="icon" href="{{ asset('favicon.ico') }}" sizes="any">
-<link rel="apple-touch-icon" href="{{ asset('favicon.ico') }}">
+<link rel="icon" href="{{ asset('favicon.ico') }}?v=1" sizes="any">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}?v=1">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}?v=1">
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}?v=1">
 
 <meta property="og:locale" content="{{ $metaLocale }}">
 <meta property="og:type" content="{{ $metaType }}">
