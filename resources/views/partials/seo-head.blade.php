4bf3668e87d9b762861609076feb0d80f855b403
ahmed-mostafa1 <ahmed.code.dev@gmail.com>
2026-05-04 19:02:32 +0000

    Fix PWA app icon manifest

--- resources/views/partials/seo-head.blade.php
+++ resources/views/partials/seo-head.blade.php
@@ -88,6 +88,7 @@
 <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}?v=1">
 <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}?v=1">
 <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}?v=1">
+<link rel="manifest" href="{{ asset('site.webmanifest') }}?v=2">
 
 <meta property="og:locale" content="{{ $metaLocale }}">
 <meta property="og:type" content="{{ $metaType }}">
